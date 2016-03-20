c77668c1ecf26d8765413d8196f1076100756ea7
Ticki <Ticki@users.noreply.github.com>
2016-03-20 22:26:15 +0100

    Avoid 'contains_key', since it is not available on Redox

--- src/map.rs
+++ src/map.rs
@@ -15,7 +15,7 @@ impl<K, V, S> MapExt<K, V, S> for HashMap<K, V, S>
     where K: Eq + Hash + Clone,
           S: BuildHasher + Default {
     fn get_or(&mut self, key: K, or: V) -> &V {
-        if self.contains_key(&key) {
+        if self.get(&key).is_some() { // TODO contains_key is currently unavailable on redox.
             self.get(&key).unwrap()
         } else {
             self.insert(key.clone(), or);
@@ -24,7 +24,7 @@ impl<K, V, S> MapExt<K, V, S> for HashMap<K, V, S>
     }
 
     fn get_or_mut(&mut self, key: K, or: V) -> &mut V {
-        if self.contains_key(&key) {
+        if self.get(&key).is_some() { // TODO contains_key is currently unavailable on redox.
             self.get_mut(&key).unwrap()
         } else {
             self.insert(key.clone(), or);
